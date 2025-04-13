6df07e88b7f81259b93d8680b7d64d6de06b06cb
wilsup2004 <wabendoh@gmail.com>
2025-04-14 01:04:19 +0200

    ajout docker file pour deploiement Railway

--- src/main/java/com/weight/services/OrangeMoneyService.java
+++ src/main/java/com/weight/services/OrangeMoneyService.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @Service
 public class OrangeMoneyService {
-    
+  /*  
     @Value("${orangemoney.api.url}")
     private String apiUrl;
     
@@ -23,7 +23,7 @@ public class OrangeMoneyService {
     
     @Value("${orangemoney.api.key}")
     private String apiKey;
-    
+ */
     private final RestTemplate restTemplate;
     
     public OrangeMoneyService() {
@@ -31,22 +31,27 @@ public class OrangeMoneyService {
     }
     
     public Map<String, Object> initiatePayment(String phoneNumber, BigDecimal amount, String reference) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("X-Auth-Token", apiKey);
+       
+    	//HttpHeaders headers = new HttpHeaders();
+      //  headers.setContentType(MediaType.APPLICATION_JSON);
+      //  headers.set("X-Auth-Token", apiKey);
         
         Map<String, Object> requestBody = new HashMap<>();
+        /*
         requestBody.put("merchantId", merchantId);
         requestBody.put("phoneNumber", phoneNumber);
         requestBody.put("amount", amount);
         requestBody.put("reference", reference);
         
         HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
-        
+      
         return restTemplate.postForObject(apiUrl + "/payments/initiate", request, Map.class);
+          */
+        return null;
     }
     
     public Map<String, Object> checkPaymentStatus(String transactionId) {
+    	/*
         HttpHeaders headers = new HttpHeaders();
         headers.set("X-Auth-Token", apiKey);
         
@@ -57,5 +62,9 @@ public class OrangeMoneyService {
             Map.class, 
             request
         );
+        
+        */
+        
+        return null;
     }
 }
